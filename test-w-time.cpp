6c75ff44c4ac26730fa0bf5ff017fb16f19218ee
Mathew <khaless@hotmail.com>
2009-04-03 10:10:22 +1100

    Cleaned up makefile and test-w-time output

--- test-w-time.cpp
+++ test-w-time.cpp
@@ -34,33 +34,33 @@ int main(void) {
 	/*
 	 * Using the Lambda Library to multiply by 10
 	 */
-	printf("=== Lambda Library \\w transform() ===\n");
+	//printf("=== Lambda Library \\w transform() ===\n");
 	gettimeofday(&tv_initial, &tz);
 	for(i = 0; i < 1000000; i++) {
 		transform(v.begin(), v.end(), v.begin(), _1 + 1);
 	}
 	gettimeofday(&tv_finish, &tz);
-	for_each(v.begin(), v.end(), cout << _1 << "\n");
-	printf("Took %d microseconds\n", ((int)(tv_finish.tv_sec - tv_initial.tv_sec) * 1000000) + (int)(tv_finish.tv_usec - tv_initial.tv_usec));
+	//for_each(v.begin(), v.end(), cout << _1 << "\n");
+	printf("%d,", ((int)(tv_finish.tv_sec - tv_initial.tv_sec) * 1000000) + (int)(tv_finish.tv_usec - tv_initial.tv_usec));
 
-	printf("=== Mult10 class with Functor ===\n");
+	//printf("=== Mult10 class with Functor ===\n");
 	gettimeofday(&tv_initial, &tz);
 	Mult10 f;
 	for(i = 0; i < 1000000; i++) {
 		transform(v.begin(), v.end(), v.begin(), f);
 	}
 	gettimeofday(&tv_finish, &tz);
-	for_each(v.begin(), v.end(), cout << _1 << "\n");
-	printf("Took %d microseconds\n", ((int)(tv_finish.tv_sec - tv_initial.tv_sec) * 1000000) + (int)(tv_finish.tv_usec - tv_initial.tv_usec));
+	//for_each(v.begin(), v.end(), cout << _1 << "\n");
+	printf("%d,", ((int)(tv_finish.tv_sec - tv_initial.tv_sec) * 1000000) + (int)(tv_finish.tv_usec - tv_initial.tv_usec));
 
-	printf("=== std::plus ===\n");
+	//printf("=== std::plus ===\n");
 	gettimeofday(&tv_initial, &tz);
 	for(i = 0; i < 1000000; i++) {
 		transform(v.begin(), v.end(), v.begin(), bind2nd(std::plus<int>(), 1));
 	}
 	gettimeofday(&tv_finish, &tz);
-	for_each(v.begin(), v.end(), cout << _1 << "\n");
-	printf("Took %d microseconds\n", ((int)(tv_finish.tv_sec - tv_initial.tv_sec) * 1000000) + (int)(tv_finish.tv_usec - tv_initial.tv_usec));
+	//for_each(v.begin(), v.end(), cout << _1 << "\n");
+	printf("%d\n", ((int)(tv_finish.tv_sec - tv_initial.tv_sec) * 1000000) + (int)(tv_finish.tv_usec - tv_initial.tv_usec));
 
 	return 0;
 }
